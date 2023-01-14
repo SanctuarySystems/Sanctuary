c50d45b0d8df47859b02b36d7d7572c46c7e32d5
Justin <just.inchong4life@gmail.com>
2023-01-14 15:29:01 -0800

    set up comment page

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Comments from './components/Comments/Comments.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Comments />
     </View>
   );
 }

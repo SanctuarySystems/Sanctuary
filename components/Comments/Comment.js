22641195c921c57758ae9ddc7b0a25ab4bd6cf51
Justin <just.inchong4life@gmail.com>
2023-01-15 21:03:13 -0800

    removed hugcount files, added usernames, added mock data, added pop and plop buttons that increase or decrease pop count, comments ordered by highest pops

--- components/Comments/Comment.js
+++ components/Comments/Comment.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { StyleSheet, View, TextInput, Text, ScrollView } from 'react-native';
-import HugCount from './HugCount';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
-const Comment = () => {
+const Comment = ({ username, body, pops }) => {
+  const [pop, setPop] = useState(pops);
 
   return (
     <View style={styles.comment}>
       <Text>I'm a comment</Text>
-      <HugCount />
+      <Text style={styles.username}>{username}</Text>
+      <Text style={styles.popCount}>{pop}</Text>
+      <Text style={styles.pop} onPress={() => setPop(pop + 1)}>pop</Text>
+      <Text style={styles.plop} onPress={() => setPop(pop - 1)}>plop</Text>
     </View>
   );
 }
@@ -15,16 +18,44 @@ const Comment = () => {
 const styles = StyleSheet.create({
   comment: {
     backgroundColor: '#FAF9F6',
-    borderRadius: 5,
-    borderWidth: 3,
+    borderRadius: '5%',
+    borderWidth: 2,
+    borderColor: 'purple',
+    shadowColor: 'purple',
+    shadowOffset: {
+      width: 10,
+      height: 10,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: '15%',
-    width: '80%',
+    padding: '20%',
+    width: '90%',
     marginBottom: '5%',
     marginLeft: 'auto',
     marginRight: 'auto',
   },
+  username: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+  },
+  popCount: {
+    position: 'absolute',
+    bottom: 10,
+    left: 10,
+  },
+  pop: {
+    position: 'absolute',
+    bottom: 10,
+    right: 50,
+  },
+  plop: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+  }
 });
 
 export default Comment;
--- components/Comments/Comments.js
+++ components/Comments/Comments.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import { StyleSheet, TextInput, Text, ScrollView, View }  from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, Text, ScrollView }  from 'react-native';
 import Comment from './Comment';
 import DetailedConfession from './DetailedConfession';
 
 const Comments = () => {
+  const [comments, setComments] = useState([
+    {username: 'notMax', body: `I'm a comment1`, pops: 3},
+    {username: 'notChad', body: `I'm a comment2`, pops: 4},
+    {username: 'notKimberly', body: `I'm a comment3`, pops: 6},
+    {username: 'notWarren', body: `I'm a comment4`, pops: 4},
+    {username: 'notSai', body: `I'm a comment5`, pops: 10},
+    {username: 'notJoseph', body: `I'm a comment6`, pops: 7},
+    {username: 'notJustin', body: `I'm a comment7`, pops: 1},
+  ]);
+
   return (
     <ScrollView style={styles.commentContainer}>
       <DetailedConfession />
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
-      <TextInput defaultValue='Add a comment'/>
+      {comments.sort((a, b) => b.pops - a.pops).map((comment, index) => <Comment username={comment.username} pops={comment.pops} body-={comment.body} key={index}/>)}
     </ScrollView>
   );
 };

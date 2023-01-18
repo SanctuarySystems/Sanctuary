d8e2351047ef0fe0e059dffc3ba5b4bc25f67ef7
Sai Vemireddy <srisaiteja@gmail.com>
2023-01-17 17:09:30 -0800

    added member list and some api implementation

--- components/Space/Space.js
+++ components/Space/Space.js
@@ -6,16 +6,34 @@ import GlobalStyles from './../GlobalStyles.js';
 import Icon from 'react-native-vector-icons/Ionicons';
 import axios from 'axios';
 import MemberInfo from './MemberInfo.js';
+import axios from 'axios';
+import MemberInfo from './MemberInfo.js';
 
+const Space = ({route, navigation}) => {
 const Space = ({route, navigation}) => {
   const [tab, setTab] = React.useState(0);
   const [leavejoin, setLeaveJoin] = React.useState(0);
   const [modalVisible, setModalVisible] = React.useState(false);
   const [writeConfession, changeWriteConfession] = React.useState('');
   const [isAdmin, setIsAdmin] = React.useState(route.params.admin);
+  const [isAdmin, setIsAdmin] = React.useState(route.params.admin);
   const [editMode, setEditMode] = React.useState(false);
   const [spaceDescription, setSpaceDescription] = React.useState('space description. space description. space description. space description.')
   const [spaceGuidelines, setSpaceGuidelines] = React.useState('GUIDELINES');
+  const [spaceMembers, setSpaceMembers] = React.useState([]);
+  const [numMembers, setNumMembers] = React.useState(0);
+
+  React.useEffect(() => {
+    axios.get(`http://ec2-52-33-56-56.us-west-2.compute.amazonaws.com:3000/spaces/${route.params.space_name}`)
+      .then((data) => {setSpaceDescription(data.data.description);
+        setSpaceGuidelines(data.data.guidelines.join('\n'));
+        setNumMembers(data.data.members.length);
+        if (isAdmin) {
+          setSpaceMembers(data.data.members);
+        }
+      }).catch((err) => console.log(err))
+
+  })
   const [spaceMembers, setSpaceMembers] = React.useState([]);
   const [numMembers, setNumMembers] = React.useState(0);
 
@@ -38,6 +56,8 @@ const Space = ({route, navigation}) => {
         <View>
           <Text style={{fontSize: '20%'}}>{route.params.space_name}</Text>
           <Text>{numMembers} Members</Text>
+          <Text style={{fontSize: '20%'}}>{route.params.space_name}</Text>
+          <Text>{numMembers} Members</Text>
         </View>
         {(leavejoin===0 && !isAdmin) && <TouchableOpacity style={styles.leavejoinContainer} onPress={() => {setLeaveJoin(1)}}>
           <Text style={styles.leavejoinText}>join</Text>
@@ -51,6 +71,7 @@ const Space = ({route, navigation}) => {
       </View>
       <View style={{flex: 0.5, marginLeft:'1%', marginRight:'1%'}} >
         <Text>{spaceDescription} </Text>
+        <Text>{spaceDescription} </Text>
       </View>
       <View style={{flex: 0.4, flexDirection: 'row', justifyContent: 'space-evenly'}}>
         <Text style={[tab !== 0? styles.unselectedTab: styles.selectedTab]} onPress={() => {setTab(0)}}>FEED</Text>
@@ -60,26 +81,11 @@ const Space = ({route, navigation}) => {
       {/* {tab === 0 && <View style={{ flex: 8, backgroundColor: 'red'}} />} */}
       {tab === 0 && <View style={{ flex: 8 }} ><Comments /></View>}
       {tab === 1 && <View style={{ flex: 8, backgroundColor: 'pink'}}><Text>{spaceGuidelines}</Text></View>}
-<<<<<<< HEAD
-      {tab === 0 && <View style={{ flex: 8 }} ><Comments /></View>}
-      {tab === 1 && <View style={{ flex: 8, backgroundColor: 'pink'}} />}
-      {tab === 2 && <View style={{ flex: 8, backgroundColor: 'blue'}} />}
-      {tab === 0 && <View style={{ flex: 8 }} ><Comments /></View>}
-      {tab === 1 && <View style={{ flex: 8, backgroundColor: 'pink'}}><Text>{spaceGuidelines}</Text></View>}
-=======
->>>>>>> 498a625 (added member list and some api implementation)
       {tab === 2 && <View style={{ flex: 8, flexDirection:'column', alignItems: 'center', width:'100%', paddingTop: '4%'}} >
         <ScrollView>
         {spaceMembers.map((member) => <MemberInfo space_name={route.params.space_name} username={member}/>)}
         </ScrollView>
         </View>}
-<<<<<<< HEAD
-      {tab === 0 && <View style={{ flex: 8 }} ><ConfessionList /></View>}
-      {tab === 1 && <View style={{ flex: 8, backgroundColor: 'pink'}}><Text>{spaceGuidelines}</Text></View>}
-      {tab === 2 && <View style={{ flex: 8, backgroundColor: 'blue'}} />}
-
-=======
->>>>>>> 498a625 (added member list and some api implementation)
       <View style={{flex: 0.5, alignItems:'center'}}>
         <TouchableOpacity onPress={() => {setModalVisible(true)}}>
           <Icon name="md-create-outline" size='35%'/>

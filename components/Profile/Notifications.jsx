7ba7def6a898418229d51b58e8e565f8daec470e
kimberlywycheung <kimberlywycheung@gmail.com>
2023-01-18 22:06:43 -0800

    final notif bug fixes for the night

--- components/Profile/Notifications.jsx
+++ components/Profile/Notifications.jsx
@@ -8,22 +8,23 @@ const Notifications = ({ route, navigation }) => {
   const [reports, setReports] = React.useState([]);
 
   React.useEffect(() => {
-    const reportArray = [];
+    let reportArray = reports.slice();
     console.log('spaces', spaces);
 
     spaces.map(async (space) => {
       await axios.get(`http://ec2-52-33-56-56.us-west-2.compute.amazonaws.com:3000/confessions?space_name=${space}&reported=true`)
         .then(({ data }) => {
           if (data[0]) {
+            reportArray = reports.slice();
             reportArray.push(data[0]);
+
             setReports(reportArray);
+            setUnreadNofits(reportArray.length);
             console.log('reportArray within notif useeffect', data[0]);
           }
         })
         .catch((err) => console.log('axios error in notifications', err));
     });
-
-    setUnreadNofits(reports.length);
   }, [route]);
 
   if (reports.length === 0) return;

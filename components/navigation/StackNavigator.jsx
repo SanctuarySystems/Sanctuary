a77f3fb07da8f1d99c4d618a319e6364a7750996
maxpeterson96 <mpeterson94@me.com>
2023-01-18 23:36:06 -0800

    login routes to home page

--- components/navigation/StackNavigator.jsx
+++ components/navigation/StackNavigator.jsx
@@ -68,22 +68,21 @@ const ProfileStackNavigator = () => {
   );
 };
 // { setUsername }
-const AuthenticateStackNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={screenOptionStyle}>
-      <Stack.Screen name="Welcome Screen" component={WelcomeScreen} />
-      <Stack.Screen name="Login Screen" component={LoginScreen} />
-      <Stack.Screen name="Sign Up Screen" component={SignUpScreen} />
-      <Stack.Screen name="Select Icon Screen" component={SelectIconScreen} />
-      <Stack.Screen name="Change Password Screen" component={ChangePasswordScreen} />
-    </Stack.Navigator>
-  );
-};
+// const AuthenticateStackNavigator = () => {
+//   return (
+//     <Stack.Navigator screenOptions={screenOptionStyle}>
+//       <Stack.Screen name="Welcome Screen" component={WelcomeScreen} />
+//       <Stack.Screen name="Login Screen" component={LoginScreen} />
+//       <Stack.Screen name="Sign Up Screen" component={SignUpScreen} />
+//       <Stack.Screen name="Select Icon Screen" component={SelectIconScreen} />
+//       <Stack.Screen name="Change Password Screen" component={ChangePasswordScreen} />
+//     </Stack.Navigator>
+//   );
+// };
 
 export {
   MainStackNavigator,
   ProfileStackNavigator,
   SpaceStackNavigator,
   SearchStackNavigator,
-  AuthenticateStackNavigator,
 };

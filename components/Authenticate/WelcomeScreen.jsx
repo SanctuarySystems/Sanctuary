ce7d83d2502203c69fbbb74e56d4e7f64abdb234
maxpeterson96 <mpeterson94@me.com>
2023-03-21 09:56:34 -0700

    adjusted background opacity

--- components/Authenticate/WelcomeScreen.jsx
+++ components/Authenticate/WelcomeScreen.jsx
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(255, 255, 255, .2)',
+    backgroundColor: 'rgba(255, 255, 255, 0)',
   },
   logoContainer: {
     marginTop: -285,
